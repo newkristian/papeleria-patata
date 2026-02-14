4fcba65c6c12ecaedbaef4c59a17341f044835d1
kristianconk <christiansoto6@gmail.com>
2026-02-13 19:37:07 -0600

    Comit inicial para proyecto backend

---
+++ backend/src/test/java/com/kristianconk/api_papeleria/ApiPapeleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristianconk.api_papeleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPapeleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
