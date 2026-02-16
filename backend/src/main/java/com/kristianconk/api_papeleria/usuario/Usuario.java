48b19df594d774df6848171c6b67f3caf00ca6e1
christian soto <christian.soto@t7internationalgroup.com>
2026-02-16 10:08:15 -0600

    UserDetails para spring security

--- backend/src/main/java/com/kristianconk/api_papeleria/usuario/Usuario.java
+++ backend/src/main/java/com/kristianconk/api_papeleria/usuario/Usuario.java
@@ -6,14 +6,19 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 // Usuario.java
 @Data
 @Entity
 @Table(name = "usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -48,5 +53,28 @@ public class Usuario {
     @UpdateTimestamp
     private LocalDateTime fechaActualizacion;
 
-    // Getters y setters
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(rol.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return activo;
+    }
 }
